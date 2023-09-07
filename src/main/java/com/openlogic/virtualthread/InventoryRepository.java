4cc62e8086f2a35e657748050d7d5d7ef235bfb6
wcrowell <wcrowell@perforce.com>
2023-09-07 03:34:17 -0400

    Create InventoryRepository.java

---
+++ src/main/java/com/openlogic/virtualthread/InventoryRepository.java
@@ -0,0 +1,6 @@
+package com.openlogic.virtualthread;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory,Long> {
+}
